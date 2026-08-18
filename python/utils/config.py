f38125b0c7576524ef5dc360e4447f27cda1617e
Tanitsorn Sathon <297323227+tanitsorn@users.noreply.github.com>
2026-08-18 12:55:10 +0700

    Add Docker support for ETL Pipeline

--- python/utils/config.py
+++ python/utils/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MYSQL_CONFIG = {
-     "host": os.getenv("MYSQL_HOST", "localhost"),
+    "host": os.getenv("MYSQL_HOST", "localhost"),
     "port": int(os.getenv("MYSQL_PORT", 3306)),
     "user": os.getenv("MYSQL_USER", "root"),
     "password": os.getenv("MYSQL_PASSWORD", ""),
